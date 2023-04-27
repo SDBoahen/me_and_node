6eb9b5ff5384583af9deb7d8e7cbb59d3bfb20be
Tonesharose31 <tonesharose@pursuit.org>
2023-04-27 11:16:49 -0400

    I did it

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪�
 
 
 const meObj = {
-    name: "Sam",
-    hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
-    favoriteColors: ["gold", "navy blue", "forest green", "red"]
+    name: "Tonesha",
+    hometown: "Brooklyn 🙅🏾‍♂️",
+    favoriteColors: ["gold", "purple", "black", "beige"]
 }
 
 
 function introduceMe (personObj){
-    // print out: "Hi, my name is {your name}, and I'm from {your hometown}"
+    console.log(`Hi, my name is ${personObj.name}, and I'm from ${personObj.hometown}.`)
+console.log(`My favorite colors are ${personObj.favoriteColors}`)
+}
+introduceMe(meObj) 
+    
+
+// print out: "Hi, my name is {your name}, and I'm from {your hometown}"
 
     // print out: "My favorite colors are {your favorite colors}"
-}
+
