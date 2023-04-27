a81e200d39c1c927e10a9daf98f28364adeb84fc
anickacodes <anickalewis@pursuit.org>
2023-04-27 11:09:37 -0400

    Changes

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪🧑‍💻🙌")
+console.log(`Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪🧑‍💻🙌`)
 
 
 
 const meObj = {
-    name: "Sam",
+    name: "Nicki",
     hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
-    favoriteColors: ["gold", "navy blue", "forest green", "red"]
+    favoriteColors: ["teal ❤", "aqua", " and moon"]
 }
 
 
 function introduceMe (personObj){
     // print out: "Hi, my name is {your name}, and I'm from {your hometown}"
 
+console.log(`Hi, my name is ${personObj.name}, and I'm from ${personObj.hometown}`)
+console.log(`My favorite colors are ${personObj.favoriteColors}`)
     // print out: "My favorite colors are {your favorite colors}"
+//console.log(introduceMe(meObj))
+
 }
+introduceMe(meObj)
+// console.log(`Hi, my name is ${meObj.name}, and I'm from ${meObj.hometown}`)
+// console.log(`My favorite colors are ${meObj.favoriteColors}`)
