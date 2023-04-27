52952ce2dd2d54530cefa29cb54cd150740569a4
Lamb144 <alvinjohnson144@gmail.com>
2023-04-27 11:58:43 -0400

    Mini-intro

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const meObj = {
     name: "Alvin Johnson",
     hometown: "Bronx, New York",
     favoriteColors: "Haze Grey",
-    favCity: "Roppongi,Japan"
+    favCity: "Roppongi,Japan",
+    favNumber: 7
 }
 
 
 function introduceMe (personObj){
     console.log(`Hello my name is ${personObj.name} and I was born and raised in ${personObj.hometown}.`);
     console.log("\r\n");
-    console.log(` My favorite color is ${personObj.favoriteColors} and my favorite city is ${personObj.favCity}.`);
+    console.log(` My favorite number is ${personObj.favNumber} my favorite color is ${personObj.favoriteColors} and my favorite city is ${personObj.favCity}.`);
 }
 introduceMe(meObj)
