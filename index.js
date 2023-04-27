aef3131fb1a214c3bbd05435f5c6c2f501d44adb
Natyka Callwood <Natykacallwood@pursuit.org>
2023-04-27 11:16:58 -0400

    changes

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪�
 
 
 const meObj = {
-    name: "Sam",
-    hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
-    favoriteColors: ["gold", "navy blue", "forest green", "red"]
+    name: "Natyka",
+    hometown: "The Virgin Islands to The Boogie-Down - BX 🙅🏾‍♂️,STAND UP",
+    favoriteColors: ["red", "yellow", "purple", "pink"]
 }
 
 
 function introduceMe (personObj){
     // print out: "Hi, my name is {your name}, and I'm from {your hometown}"
+console.log(`Hi, my name is ${personObj.name}, and I'm from ${personObj.hometown}.`)
 
     // print out: "My favorite colors are {your favorite colors}"
+console.log(`My favorite colors are ${personObj.favoriteColors}.`)
 }
+
+introduceMe(meObj)
