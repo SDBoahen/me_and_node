75002f867350920414945b7ecb2199e635657117
Anthony O <antman@ANTMANS-Air.lan>
2023-04-27 11:36:44 -0400

    new commit

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪�
 
 
 const meObj = {
-    name: "Sam",
-    hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
-    favoriteColors: ["gold", "navy blue", "forest green", "red"]
+    name: "AntMan",
+    hometown: "QUEENS💸",
+    favoriteColors: ["pink", "pink", "pink", "black"]
 }
 
 
 function introduceMe (personObj){
-    // print out: "Hi, my name is {your name}, and I'm from {your hometown}"
+    console.log("Hi, my name is $(personObj.name), and I'm from $(personObj.hometown)")
+    console.log('My favorite colors are $(personobj.favoriteColors}')
 
-    // print out: "My favorite colors are {your favorite colors}"
 }
+console.log(introduceMe(meobj));
