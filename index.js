78afb38bad54ed41486749f3b6ada367192047ce
Lamb144 <alvinjohnson144@gmail.com>
2023-04-27 11:51:38 -0400

    Mini-Intro Activity

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪�
 
 const meObj = {
     name: "Alvin Johnson",
-    hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
-    favoriteColors: "Haze Grey"
+    hometown: "Bronx, New York",
+    favoriteColors: "Haze Grey",
+    favCity: "Roppongi,Japan"
 }
 
 
 function introduceMe (personObj){
     console.log(`Hello my name is ${personObj.name} and I was born and raised in ${personObj.hometown}.`);
     console.log("\r\n");
-    console.log(` My favorite color is ${personObj.favoriteColors}.`);
+    console.log(` My favorite color is ${personObj.favoriteColors} and my favorite city is ${personObj.favCity}.`);
 }
 introduceMe(meObj)
