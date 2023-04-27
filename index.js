bfd5dd21485c175d2f7b705a17c900f2e124765f
Alexandria Brinson <127881479+alexandriabrinson@users.noreply.github.com>
2023-04-27 10:56:20 -0400

    Updated me_and_node index.js
    Updated the file to include my information and create a function that would successfully print both desired lines to the console.

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪�
 
 
 const meObj = {
-    name: "Sam",
-    hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
-    favoriteColors: ["gold", "navy blue", "forest green", "red"]
-}
+    name: "Alex",
+    hometown: "the Bronx!",
+    favoriteColors: ["pink", "lavender", "olive green", "grey"]
+};
 
+function introduceMe(meObj){
+    console.log(`Hi, my name is ${meObj.name}, and I'm from ${meObj.hometown}`);
 
-function introduceMe (personObj){
-    // print out: "Hi, my name is {your name}, and I'm from {your hometown}"
+    console.log(`My favorite colors are ${meObj.favoriteColors[0]}, ${meObj.favoriteColors[1]}, ${meObj.favoriteColors[2]}, and ${meObj.favoriteColors[3]}.`);
+};
 
-    // print out: "My favorite colors are {your favorite colors}"
-}
+introduceMe(meObj);
