e13850e0d949b87963afc3c81fd27c4d33a6e968
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-04-27 11:12:42 -0400

    Complete  function

--- index.js
+++ index.js
@@ -10,5 +10,12 @@ const meObj = {
 
 function introduceMe(personObj) {
 	// print out: "Hi, my name is {your name}, and I'm from {your hometown}"
+	console.log(`Hi, my name is ${meObj.name}, and I'm from ${meObj.hometown}.`);
+
 	// print out: "My favorite colors are {your favorite colors}"
+	console.log(
+		`My favorite colors are ${meObj.favoriteColors[0]} and ${meObj.favoriteColors[1]}`
+	);
 }
+
+console.log(introduceMe(meObj));
